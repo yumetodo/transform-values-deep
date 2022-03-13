039a91a18db5c0cad06438b699ddb3e585fff195
yumetodo <yume-wikijp@live.jp>
2022-03-14 03:57:44 +0900

    refactor(test): actualとexpectedが逆だった

--- test/index.ts
+++ test/index.ts
@@ -2,28 +2,27 @@ import test from 'ava';
 import { transformAnyValuesDeep, transformValuesDeep } from '../src/index';
 test('string to number', t => {
   const foo = () => true;
-  t.deepEqual(
+  const actual = transformAnyValuesDeep(
     {
       a: 4,
       b: {
-        c: 7,
+        c: '7',
         f: foo,
       },
-      d: 9,
+      d: '9',
     },
-    transformAnyValuesDeep(
-      {
-        a: 4,
-        b: {
-          c: '7',
-          f: foo,
-        },
-        d: '9',
-      },
-      (s: string) => parseInt(s),
-      (o: unknown): o is string => typeof o === 'string'
-    )
+    (s: string) => parseInt(s),
+    (o: unknown): o is string => typeof o === 'string'
   );
+  const expected = {
+    a: 4,
+    b: {
+      c: 7,
+      f: foo,
+    },
+    d: 9,
+  };
+  t.deepEqual(actual, expected);
 });
 test('no change object', t => {
   const o = {};
